eba0de8f697414a452036df86cb5bc07dab0f10d
Aziz Miah <azizulmiah@hotmail.com>
2022-10-20 19:20:00 +0100

    food

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,28 @@
-Rails.application.routes.draw do
+# Rails.application.routes.draw do
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
   # root "articles#index"
+
+
+Rails.application.routes.draw do
+  root to: "restaurants#index"
+  resources :restaurants, only: [:index, :show, :new, :create] do
+    resources :reviews, only: [:create]
+  end
 end
+  # #read
+  # get "restaurants", to: "restaurants#index"
+  # get "restaurants/:id", to: "restaurants#show"
+
+  # #new
+  # get "restaurants/new", to: "restaurants#new"
+  # #post
+  # post "restaurants", to: "restaurants#create"
+
+  # #update
+  # get "restaurants/:id/edit", to: "restaurants#edit"
+  # patch "restaurants/:id", to: "restaurants#update"
+
+  # #delete
+  # delete "restaurants/:id", to: "restaurants#destroy"
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,21 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+
+#Restaurant.create(name:'Food place', address:'London', phone_number:'123', category:'fastfood')
+
+require 'faker'
+
+5.times do
+  category = ["chinese", "italian", "japanese", "french", "belgian"]
+  new_restaurant = Restaurant.create(
+    name: Faker::Restaurant.name,
+    address: Faker::Address.street_address,
+    category: category.sample,
+    phone_number: Faker::PhoneNumber.cell_phone_in_e164
+  )
+  new_restaurant.save
+end
+# Faker::Name.name      #=> "Christophe Bartell"
+
+# Faker::Internet.email #=> "kirsten.greenholt@corkeryfisher.info"
